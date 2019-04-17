72e98a6b0dad52afbbb99158fb0c595930657307
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-04-17 22:47:09 +0000

    For the home page, created static team list and team buttons

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Nav from './nav';
+import HomeTeamList from './home_team_list';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
+
+
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Nav />
+                <HomeTeamList />
             </div>
         )
     }

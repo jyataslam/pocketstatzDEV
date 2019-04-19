31500967a4aa5ddcc9692ae3bca27e01fb3f28b8
Jason <jasonyata@gmail.com>
2019-04-18 21:06:28 -0700

    created overwatch team page, ready for dummy data to display set up dynamic routing for nba teams added 'quicksand' font

---
+++ src/components/browse_overwatch/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import OWTeamList from './ow_team_list';
+import './browse.scss';
+
+export default props => {
+    return (
+        <div className="browse">
+            <Route path="/overwatch-team-list" exact component={OWTeamList}/>
+        </div>
+    )
+}

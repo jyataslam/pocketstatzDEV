cdf1032b22471af9635d611535b44424ca4da6a7
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-04-18 19:39:11 -0700

    Created log-in page inputs for username and password submit button placeholder for logo

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import BrowseSports from './browse_sports';
 import BrowseRoutes from './browse_teams/index';
 import HomeTeamList from './home_team_list';
+import LogIn from './log_in';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
@@ -20,6 +21,7 @@ class App extends Component {
                         <Route path="/" exact component={HomeTeamList} />
                         <Route path="/browse" component={BrowseSports} />
                         <Route path="/nba-team-list" component={BrowseRoutes} />
+                        <Route path="/log-in" component={LogIn} />
                     </Switch>
                 </div>
             </div>

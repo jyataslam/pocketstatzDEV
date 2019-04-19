afce4383313812a2731031e603c86d24b5b07b98
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-19 09:34:45 -0700

    removed comments

--- public/api/getteam.php
+++ public/api/getteam.php
@@ -1,13 +1,10 @@
 <?php 
 
-//will this be the endpoint that lists all the teams on the specific league page?
-
 require_once('functions.php');
 set_exception_handler('handleError');
 require_once('config.php');
 require_once('mysqlconnect.php');
 
-//figure out how to make this dynamic based on the button pressed
 $sport_type = 'NBA';
 
 $query = "SELECT t.`id`, t.`team_full_name`, t.`team_name`, t.`team_code`, t.`colors`, t.`image_url`
@@ -22,7 +19,6 @@ if(!$result){
 	throw new Exception('invalid query: '. mysqli_error($conn));
 }
 
-//is this necessary? come back to it
 if(mysqli_num_rows($result) === 0) {
     throw new Exception('unable to retrieve team data');
 }

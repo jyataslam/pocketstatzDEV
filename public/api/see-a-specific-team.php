ce5e7d47a3c652898beecbe8346179ddd1d03707
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-22 12:52:02 -0700

    added an endpoint to see the info from a specific team

---
+++ public/api/see-a-specific-team.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once('functions.php');
+set_exception_handler('handleError');
+require_once('config.php');
+require_once('mysqlconnect.php');
+
+//needs the team_id
+if(empty($_GET['team_id'])){
+    throw new Exception('team_id is a required value');
+}
+
+$team = $_GET['team_id'];
+
+$query = "SELECT t.`id`, t.`team_full_name`, t.`team_name`, t.`team_code`, t.`colors`, t.`league_name`, t.`image_url`
+	FROM `teams` AS t
+    WHERE t.`id` = $team
+";
+
+$data = mysqli_query($conn, $query);
+
+if(!$data){
+    throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_num_rows($data) === 0) {
+    throw new Exception('Unable to retrieve team data');
+}
+
+$output = [];
+
+while($row = mysqli_fetch_assoc($data)){
+    $output = [
+        'team_id' => (int) $row['id'],
+        'team_full_name' => $row['team_full_name'],
+        'team_name' => $row['team_name'],
+        'team_code' => $row['team_code'],
+        'logo' => $row['image_url'],
+        'colors' => $row['colors'],
+        'league_name' => $row['league_name']
+    ];
+}
+
+$output['success'] = true;
+
+print(json_encode($output));
+
+?>

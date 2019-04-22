f622ef5cac7e0173206866b88e6d2a54bbbdc0a9
Jason <jasonyata@gmail.com>
2019-04-22 10:00:35 -0700

    changed background color for team buttons changed landing page button colors

--- src/components/landing_page/index.js
+++ src/components/landing_page/index.js
@@ -7,17 +7,17 @@ export default props => {
         <div>
             <div className=" welcome-header center">Welcome to Pocket Statz!</div>
             <div className="divider"></div>
-            <p className="center">Get the latest stats of your favorite sport teams</p>
+            <p className="center">Get the latest stats from your favorite sport teams</p>
             <div className="row">
-                <Link to="/log-in" className="center btn  col s4 offset-s1 hide-on-med-and-up">Login</Link>                
-                <Link to="/log-in" className="center btn col s4 offset-s2 hide-on-med-and-up">Sign-up</Link>               
-                <Link to="/log-in" className="center btn btn-large col m3 offset-m3 hide-on-small-only">Login</Link>
-                <Link to="/log-in" className="center btn btn-large col  m3 hide-on-small-only">Sign-up</Link>
+                <Link to="/log-in" className="center btn  col s4 offset-s1 hide-on-med-and-up grey darken-3">Login</Link>                
+                <Link to="/log-in" className="center btn col s4 offset-s2 hide-on-med-and-up grey darken-3">Sign-up</Link>               
+                <Link to="/log-in" className="center btn btn-large col m3 offset-m3 hide-on-small-only grey darken-3">Login</Link>
+                <Link to="/log-in" className="center btn btn-large col  m3 hide-on-small-only grey darken-3">Sign-up</Link>
             </div>
 
             <div className="row">
-                <Link to="/browse" className="center btn col s4 offset-s4 hide-on-med-and-up">Guest</Link>
-                <Link to="/browse" className="center btn btn-large col m4 offset-m4 hide-on-small-only">Guest</Link>
+                <Link to="/browse" className="center btn col s4 offset-s4 hide-on-med-and-up grey">Guest</Link>
+                <Link to="/browse" className="center btn btn-large col m4 offset-m4 hide-on-small-only grey">Guest</Link>
             </div>
         </div>
         

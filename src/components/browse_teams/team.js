240fba9ecad34431fc7af8db15e7fa068aaeb3d6
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-23 16:44:29 -0700

    bundled for deployment

--- src/components/browse_teams/team.js
+++ src/components/browse_teams/team.js
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react';
 
 class TeamSquare extends Component {
 
+    componentDidMount() {
+        M.Dropdown.init(this.dropdown);
+    }
+
     replaceSpaceWithDash = (str) => {
         let newStr = "";
 
@@ -17,35 +21,24 @@ class TeamSquare extends Component {
     }
 
     render() {
-        const { checkStats, chooseTeam, id, api_key, team_full_name } = this.props;
+        const { chooseTeam, id, api_key, team_full_name } = this.props;
         const teamName = this.replaceSpaceWithDash(team_full_name);
         const teamLogo = require(`../../assets/images/${teamName}.png`);
-        const fontSize = { "fontSize": "14px" };
 
-        return (
-            <Fragment>
-                <div className="team-container col s6 m3" >
-                    <button className="team-item z-depth-3" onClick={() => { chooseTeam(id) }}>
-                        <img src={teamLogo} />
-                    </button>
-                </div>
-            </Fragment>
-        )
-    }
-}
-
-export default TeamSquare;
-
-{/* <Fragment>
+            return (
+                <Fragment>
                     <div className="team-container col s6 m3" >
                         <button ref={(element) => { this.dropdown = element }} className="team-item dropdown-trigger" data-target="dropdown1">
                             <img src={teamLogo} />
                         </button>
-                        <ul  id="dropdown1" className="dropdown-content">
-                        <li><a><i onClick={() => { debugger;console.log('yo');chooseTeam(id) }} className="material-icons">add</i><span style={fontSize}>Add to My-Teams</span></a></li>
-                        <li className="divider" tabIndex="-1"></li>
-                        <li><a><i onClick={() => { checkStats(id) }} className="material-icons">arrow_forward</i><span style={fontSize}>Check Stats</span></a></li>
-                    </ul>
                     </div>
-                   
-                </Fragment> */}
+                    <ul  id="dropdown1" className="dropdown-content">
+                        <li><a onClick={() => { chooseTeam(id) }}><i className="material-icons">add</i></a></li>
+                        <li><a><i className="material-icons">cloud</i>five</a></li>
+                    </ul>
+                </Fragment>
+            )
+    }
+}
+
+export default TeamSquare;

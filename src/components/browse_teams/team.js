41d960042b229f871dc095638d5a664f86ab1ad5
Jason <jasonyata@gmail.com>
2019-04-23 18:03:48 -0700

    team.js file updated did not update and merge correctly, don't know why

--- src/components/browse_teams/team.js
+++ src/components/browse_teams/team.js
@@ -35,17 +35,3 @@ class TeamSquare extends Component {
 }
 
 export default TeamSquare;
-
-{/* <Fragment>
-                    <div className="team-container col s6 m3" >
-                        <button ref={(element) => { this.dropdown = element }} className="team-item dropdown-trigger" data-target="dropdown1">
-                            <img src={teamLogo} />
-                        </button>
-                        <ul  id="dropdown1" className="dropdown-content">
-                        <li><a><i onClick={() => { debugger;console.log('yo');chooseTeam(id) }} className="material-icons">add</i><span style={fontSize}>Add to My-Teams</span></a></li>
-                        <li className="divider" tabIndex="-1"></li>
-                        <li><a><i onClick={() => { checkStats(id) }} className="material-icons">arrow_forward</i><span style={fontSize}>Check Stats</span></a></li>
-                    </ul>
-                    </div>
-                   
-                </Fragment> */}

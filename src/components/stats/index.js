6b70a2b617ff83f005963cb289d9c77cdd448de9
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-04-25 23:15:57 +0000

    loading screen reducer created game stats reducer created installed thunk middleware game-stats-page now displays loadigscreen while waiting for axios response then renders game stats

--- src/components/stats/index.js
+++ src/components/stats/index.js
@@ -3,6 +3,9 @@ import TeamScore from './team_score/team_score';
 import TeamsTab from './teams_tab/teams_tab';
 import axios from 'axios';
 import PlayerStats from './players_stats/players_stats';
+import {connect} from "react-redux";
+import {gameInfo, loadStart, loadEnd} from "../../actions";
+import LoadingScreen from "../loading_screen";
 
 class GameInfo extends Component {
 
@@ -14,27 +17,46 @@ class GameInfo extends Component {
         gameDetails: null
     }   
 
-    componentDidMount(){
-        this.getGameStats();  
+    async componentDidMount(){
+        this.props.loadStart();
+        await this.props.gameInfo(this.props);
+        this.props.loadEnd();
+        // this.getGameStats();  
     }
 
-    getGameStats(){
-        axios.get(`/api/see-a-specific-team.php?team_id=${this.props.match.params.team_id}`).then((resp) => {
-            axios.get(`/api/getnbagameid.php?team_name=${resp.data.api_key}`).then((resp) => {
-                if (typeof resp.data === 'object'){
-                this.setState({
-                    team1: resp.data.awayTeam,
-                    team2: resp.data.homeTeam,
-                    gameDetails: resp.data.gameDetails,
-                    isLoaded: true
-                });
-                } else {
-                    // make axios call to get last game available from regular season and setState as above
-                    console.log('no team data available');
-                }
-            })
-        });
-    }
+    // async getGameStats(){
+
+    //     const resp = await axios.get(`/api/see-a-specific-team.php?team_id=${this.props.match.params.team_id}`);
+    //     const resp2 = await axios.get(`/api/getnbagameid.php?team_name=${resp.data.api_key}`);
+        
+    //     if (typeof resp2.data === 'object'){
+    //         this.setState({
+    //             team1: resp2.data.awayTeam,
+    //             team2: resp2.data.homeTeam,
+    //             gameDetails: resp2.data.gameDetails,
+    //             isLoaded: true
+    //         });
+    //         } else {
+    //             // make axios call to get last game available from regular season and setState as above
+    //             console.log('no team data available');
+    //         }
+
+    //     // axios.get(`/api/see-a-specific-team.php?team_id=${this.props.match.params.team_id}`).then((resp) => {
+    //     //     axios.get(`/api/getnbagameid.php?team_name=${resp.data.api_key}`).then((resp) => {
+    //     //         if (typeof resp.data === 'object'){
+    //     //         this.setState({
+    //     //             team1: resp.data.awayTeam,
+    //     //             team2: resp.data.homeTeam,
+    //     //             gameDetails: resp.data.gameDetails,
+    //     //             isLoaded: true
+    //     //         });
+    //     //         } else {
+    //     //             // make axios call to get last game available from regular season and setState as above
+    //     //             console.log('no team data available');
+    //     //         }
+    //     //     })
+    //     // });
+    // }
 
     showLeft = () => {
         this.setState({
@@ -49,18 +71,33 @@ class GameInfo extends Component {
     }
 
     render(){
-        const {view, team1, team2, gameDetails, isLoaded} = this.state;
+        const {view, team1, team2, gameDetails} = this.props.gameStats;
         const {showLeft, showRight} = this;
+
+        if (!this.props.isLoaded) {
+            return <LoadingScreen />
+        }
+
         return(
             
             <Fragment>
-                {isLoaded && <TeamScore team1={team1} team2={team2} gameDetails={gameDetails}/>}
-                {isLoaded && <TeamsTab team1={team1} team2={team2} showLeft={showLeft} showRight={showRight}/>}
-                {isLoaded && <PlayerStats view={view} team1={team1} team2={team2}/>}
+                <TeamScore team1={team1} team2={team2} gameDetails={gameDetails}/>
+                <TeamsTab team1={team1} team2={team2} showLeft={showLeft} showRight={showRight}/>
+                <PlayerStats view={view} team1={team1} team2={team2}/>
             </Fragment> 
             
         );
     }
 }
 
-export default GameInfo;
+function mapStateToProps(state)
+{
+    return{
+        isLoaded: state.loading.isLoaded,
+        gameStats: state.stats
+    }
+}
+
+export default connect(mapStateToProps, {
+    gameInfo, loadStart, loadEnd
+})(GameInfo);

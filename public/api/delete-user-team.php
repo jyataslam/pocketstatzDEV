89e8560993017e7d7b7687a7d270bb5b8c931da9
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-25 16:11:15 -0700

    added delete user endpoint

---
+++ public/api/delete-user-team.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('functions.php');
+set_exception_handler('handleError');
+require_once('config.php');
+require_once('mysqlconnect.php');
+
+$user_id = 1;
+
+if(empty($_GET['team_id'])){
+	throw new Exception('You must send a team_id with your request');
+}
+
+$team_id = $_GET['team_id'];
+$user_id = $_GET['user_id'];
+
+$query = "DELETE FROM `user_teams`
+    WHERE `user_id` = $user_id AND `team_id` = $team_id";
+
+$result = mysqli_query($conn, $query);
+
+if(!$result){
+	throw new Exception(mysqli_error($conn));
+}
+
+$output = [
+	'success' => true
+];
+
+print( json_encode($output));
+?>

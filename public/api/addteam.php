a4725bd2abdc04f0086ea1f9e7bf0e2369fb74ca
Michelle Poole <michelle.poole.email@gmail.com>
2019-04-29 13:47:30 -0700

    updated addteams query to handle the addition of multiple teams verified query success through postman

--- public/api/addteam.php
+++ public/api/addteam.php
@@ -5,23 +5,18 @@ set_exception_handler('handleError');
 require_once('config.php');
 require_once('mysqlconnect.php');
 
-// $_SESSION['user_data'] = [
-//     'id' => $data['id'],
-//     'username' => $data['username'],
-//     'token' => $token
-// ];
-
 $user_id = $_SESSION['user_data']['id'];
 
 if(empty($_GET['team_id'])){
 	throw new Exception('You must send a team_id with your request');
 }
 
-$team_id = $_GET['team_id'];
-// $id_explode = explode(",",$team_id);
-// $id_list = implode(" OR ", $id_explode);
+$teams = $_GET['team_id'];
 
-$query = "SELECT `team_full_name`, `team_name`, `team_code`, `image_url`, `colors`, `league_name` FROM `teams` WHERE `id` = $team_id";
+$query = "SELECT `team_full_name`, `team_name`, `team_code`, `image_url`, `colors`, `league_name`
+	FROM `teams`
+	WHERE `id` IN ($teams)";
+
 $result = mysqli_query($conn, $query);
 
 if(!$result){
@@ -34,35 +29,30 @@ if( mysqli_num_rows($result) === 0){
 
 $team_data = mysqli_fetch_assoc($result);
 
-print_r($team_data);
-
-if(empty($_SESSION['user_data'])){
-	// $homepage_create_query = "INSERT INTO `user_teams` SET 
-	// 	`user_id` = $user_id,
-	// 	`team_id` = $team_id
-	// ";
-	// $homepage_result = mysqli_query($conn, $homepage_create_query);
-
+if(!$user_id){
 	throw new Exception("You need to be logged in to add teams");
 	if(!$homepage_result){
 		throw new Exception(mysqli_error($conn));
 	}
+
 	if(mysqli_affected_rows($conn)===0){
 		throw new Exception('data was not added to user_teams table');
 	}
-	// $homepage_id = mysqli_insert_id($conn);
-	// $_SESSION['homepage_id'] = $homepage_id;
 } else {
-	$homepage_create_query = "INSERT INTO `user_teams` SET 
-		`user_id` = $user_id,
-		`team_id` = $team_id
-	";
-	$homepage_result = mysqli_query($conn, $homepage_create_query);
-	if(!$homepage_result){
-		throw new Exception(mysqli_error($conn));
-	}
-	if(mysqli_affected_rows($conn)===0){
-		throw new Exception('data was not added to user_teams table');
+	$team_array = explode(",", $teams);
+
+	foreach ($team_array as $value) {
+		$homepage_create_query = "INSERT INTO `user_teams` (`user_id`, `team_id`)
+			VALUES ($user_id, $value)";
+
+		$homepage_result = mysqli_query($conn, $homepage_create_query);
+
+		if(!$homepage_result){
+			throw new Exception(mysqli_error($conn));
+		}
+		if(mysqli_affected_rows($conn)===0){
+			throw new Exception('data was not added to user_teams table');
+		}
 	}
 }
 

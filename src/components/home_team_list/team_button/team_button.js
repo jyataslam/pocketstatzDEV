36430646c075ffd03cbb22ea72d57fa0499de46c
Christopher-Gustavsson <chrisgustavsson1@gmail.com>
2019-04-28 22:36:37 -0700

    Display game stats hotfix Fixed a bug where only logged in or guest user's game stats would load. Either logged in or guest user's game stats now load correctly

--- src/components/home_team_list/team_button/team_button.js
+++ src/components/home_team_list/team_button/team_button.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './team_button.scss';
 
 export default (props) => {
-    const { colors, team_full_name, id, chooseTeam, league_name } = props;
-    console.log("Team button props:", props);
+    const { colors, team_full_name, team_id, id ,chooseTeam, league_name } = props;
+    
     const backgroundColor = {'backgroundColor': `${colors}`};
 
         return(
             <div className="container">
                 <div className="row">
-                    <button onClick={() => {chooseTeam(id, league_name)}} style={backgroundColor} id="tabs-swipe-demo" className="btn btn-large home-team-button col s12">{team_full_name}</button>
+                    <button onClick={() => {chooseTeam(team_id || id, league_name)}} style={backgroundColor} id="tabs-swipe-demo" className="btn btn-large home-team-button col s12">{team_full_name}</button>
                 </div>
             </div>
         );

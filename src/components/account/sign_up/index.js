31de8eb95473f0b5e176147a08baf9ccf53fd1c1
Jason <jasonyata@gmail.com>
2019-04-29 19:31:40 -0700

    validation in progress

--- src/components/account/sign_up/index.js
+++ src/components/account/sign_up/index.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import SignUpForm from './sign_up_form';
 
 class SignUp extends Component {
+    handleSignUp(values) {
+        console.log('values: ', values);
+    }
+
     render() {
         return (
             <div>
                 <h3 className="center">Logo goes here</h3>
                 <h4 className="center">Create an account</h4>
-                <SignUpForm />
+                <SignUpForm signUp={this.handleSignUp}/>
             </div>
         );
     }

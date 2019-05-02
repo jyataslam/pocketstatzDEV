a784d838ce76a6bc04d147dc84dbc4d1085d96d6
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-05-02 00:29:32 +0000

    signed i in users' teams are now stored in redux-store, cutting their loading time

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,6 +2,8 @@ export default {
     SIGN_IN_USER: 'sign_in_user',
     SIGN_OUT_USER: 'sign_out_user',
     SIGN_UP_USER: 'sign_up_user',
+    RETRIEVE_USER_TEAMS: 'retrieve_user_teams',
+    DELETE_USER_TEAM: 'delete_user_team',
     RETRIEVE_STATS: 'retrieve_stats',
     RETRIEVE_TEAMS: 'retrieve_teams',
     LOAD_START: 'load_start',

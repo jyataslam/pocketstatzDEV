d65bc176fea3246d97a5e3b18bcba3b555dbea16
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-05-02 23:32:07 +0000

    changed search-icon to an add-icon

--- src/components/nav/nav_footer.js
+++ src/components/nav/nav_footer.js
@@ -12,7 +12,7 @@ class NavFooter extends Component {
                     <div className="col s12" style={padding}>
                         <ul className="tabs tabs-fixed-width transparent white-text">
                             <li className="tab col s4 white-text"><Link to="/user-teams" className="white-text"><i className="material-icons">home</i></Link></li>
-                            <li className="tab col s4"><Link to="/browse" className="white-text"><i className="material-icons">search</i></Link></li>
+                            <li className="tab col s4"><Link to="/browse" className="white-text"><i className="material-icons">add_circle</i></Link></li>
                             <li className="tab col s4"><Link to="/account/sign-in" className="white-text"><i className="material-icons">account_circle</i></Link></li>
                         </ul>
                     </div>

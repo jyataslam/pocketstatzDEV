22e2473ceafc72433b942a79db3b811badb7ae81
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-05-02 20:07:16 +0000

    updated the game stats tab buttons on nhl games to be the same as nba games

--- src/components/nhl_stats/teams_tab/teams_tab.js
+++ src/components/nhl_stats/teams_tab/teams_tab.js
@@ -3,14 +3,11 @@ import React, {Component} from 'react';
 
 class TeamsTab extends Component {
     render(){
-        const margin = {"margin": "10px 0"};
-        const height = {"height": "20px", "lineHeight": "inherit"};
-
         return(
             <div id="buttons-container">
-                <div className="row" style={margin}>
-                        <button className="btn btn-small col s4 offset-s1" style={height} onClick={this.props.showLeft}>{this.props.team1.teamName}</button>
-                        <button className="btn btn-small col s4 offset-s2" style={height} onClick={this.props.showRight}>{this.props.team2.teamName}</button>
+                <div className="row" id="row">
+                        <button className="btn col s6"  onClick={this.props.showLeft}>{this.props.team1.teamName}</button>
+                        <button className="btn col s6"  onClick={this.props.showRight}>{this.props.team2.teamName}</button>
                 </div>
             </div>
         );

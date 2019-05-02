4b9dc035657a950d45951ca994f9dd6f48aa06b1
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-05-02 04:42:42 +0000

    modified the css for landing page buttons

--- src/components/landing_page/index.js
+++ src/components/landing_page/index.js
@@ -9,15 +9,15 @@ export default props => {
             <div className="divider"></div>
             <p className="center">Get the latest stats from your favorite sport teams</p>
             <div className="row">
-                <Link to="/account/sign-in" className="center btn  col s4 offset-s1 hide-on-med-and-up grey darken-3">Sign In</Link>                
-                <Link to="/account/sign-up" className="center btn col s4 offset-s2 hide-on-med-and-up grey darken-3">Sign Up</Link>               
-                <Link to="/account/sign-in" className="center btn btn-large col m3 offset-m3 hide-on-small-only grey darken-3">Sign In</Link>
-                <Link to="/account/sign-up" className="center btn btn-large col  m3 hide-on-small-only grey darken-3">Sign Up</Link>
+                <Link to="/account/sign-in" id="landing-btn" className="center btn  col s4 offset-s1 hide-on-med-and-up orange darken-3">Sign In</Link>                
+                <Link to="/account/sign-up" id="landing-btn" className="center btn col s4 offset-s2 hide-on-med-and-up orange darken-3">Sign Up</Link>               
+                <Link to="/account/sign-in" id="landing-btn" className="center btn btn-large col m3 offset-m3 hide-on-small-only orange darken-3">Sign In</Link>
+                <Link to="/account/sign-up" id="landing-btn" className="center btn btn-large col m3 hide-on-small-only orange darken-3">Sign Up</Link>
             </div>
 
             <div className="row">
-                <Link to="/browse" className="center btn col s4 offset-s4 hide-on-med-and-up grey">Guest</Link>
-                <Link to="/browse" className="center btn btn-large col m4 offset-m4 hide-on-small-only grey">Guest</Link>
+                <Link to="/browse" id="landing-btn-guest" className="center btn col s4 offset-s4 hide-on-med-and-up">Guest</Link>
+                <Link to="/browse" id="landing-btn-guest" className="center btn btn-large col m4 offset-m4 hide-on-small-only">Guest</Link>
             </div>
         </div>
         

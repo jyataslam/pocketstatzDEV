e80858ce23bb2310ab0b6e386f47e58fc36b2e16
Jason <jasonyata@gmail.com>
2019-05-10 14:50:21 -0700

    fixed background image bleeding issue onto teams stats page removed thin divider lines in nhl player stats table

--- src/components/browse_teams/index.js
+++ src/components/browse_teams/index.js
@@ -9,19 +9,20 @@ import './browse.scss';
 export default props => {
     return (
         <div id="browse" className="browse-container">
-            
             <Route path="/browse/nba" render={(routingProps) => {
-                    return <TeamList {...routingProps} leagueName = 'nba'/>
-                }}/>
+                return <TeamList {...routingProps} leagueName='nba' />
+            }} />
             <Route path="/browse/nhl" render={(routingProps) => {
-                    return <TeamList {...routingProps} leagueName = 'nhl'/>
-                }}/>
+                return <TeamList {...routingProps} leagueName='nhl' />
+            }} />
+        <div id="teams-main">
             <Route path="/nba/:team_id" render={(routingProps) => {
-                    return <NBAGameInfo {...routingProps}/>
-                }}/>
+                return <NBAGameInfo {...routingProps} />
+            }} />
             <Route path="/nhl/:team_id" render={(routingProps) => {
-                    return <NHLGameInfo {...routingProps}/>
-                }}/>
+                return <NHLGameInfo {...routingProps} />
+            }} />
+        </div>
         </div>
     )
 }
--- src/components/landing_page/index.js
+++ src/components/landing_page/index.js
@@ -11,16 +11,16 @@ export default props => {
             <p className="center welcome-text">The Latest Stats From Your Favorite Teams</p>
             <section className="landing-buttons">
                 <div className="row signup-buttons">
-                    <Link to="/account/sign-in" id="landing-btn" className="center btn col s4 offset-s1 hide-on-med-and-up orange darken-3">Sign In</Link>
+                    <Link to="/account/sign-in" id="landing-btn" className="center btn col s4 offset-s1 hide-on-med-and-up orange darken-3">Log In</Link>
                     <Link to="/account/sign-up" id="landing-btn" className="center btn col s4 offset-s2 hide-on-med-and-up orange darken-3">Sign Up</Link>
 
-                    <Link to="/account/sign-in" id="landing-btn" className="center btn btn-large col m3 offset-m2 hide-on-small-only orange darken-3">Sign In</Link>
-                    <Link to="/account/sign-up" id="landing-btn" className="center btn btn-large col m3 offset-m2 hide-on-small-only orange darken-3">Sign Up</Link>
+                    <Link to="/account/sign-in" id="landing-btn" className="center btn col m3 offset-m2 hide-on-small-only orange darken-3">Log In</Link>
+                    <Link to="/account/sign-up" id="landing-btn" className="center btn col m3 offset-m2 hide-on-small-only orange darken-3">Sign Up</Link>
                 </div>
 
                 <div className="row guest-container">
                     <Link to="/browse" id="landing-btn-guest" className="center btn col s4 offset-s4 hide-on-med-and-up">Guest</Link>
-                    <Link to="/browse" id="landing-btn-guest" className="center btn btn-large col m4 offset-m4 hide-on-small-only">Guest</Link>
+                    <Link to="/browse" id="landing-btn-guest" className="center btn col m4 offset-m4 hide-on-small-only">Guest</Link>
                 </div>
             </section>
         </div>
